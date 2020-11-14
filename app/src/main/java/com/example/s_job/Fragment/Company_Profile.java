e392ec5c2f803c5b42071d0a47f1786be0ffe0ac
nhok21074 <nhok210724@gmail.com>
2020-11-14 10:58:21 +0700

    company

---
+++ app/src/main/java/com/example/s_job/Fragment/Company_Profile.java
@@ -0,0 +1,101 @@
+package com.example.s_job.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.s_job.R;
+import com.example.s_job.favoritedJobs;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+
+public class Company_Profile extends Fragment {
+    private LinearLayout editProfile, changePassword, favoritedJob, posts, logout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fr_company_profile, container, false);
+        init(view);
+
+
+        return view;
+    }
+
+    private void init(View view) {
+        editProfile = view.findViewById(R.id.editProfile);
+        changePassword = view.findViewById(R.id.change_Password);
+        favoritedJob =  view.findViewById(R.id.favorited_jobs);
+//        posts = findViewById(R.id.your_Posts);
+        logout = view.findViewById(R.id.logout);
+
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                        view.getContext(), R.style.BottomSheetDialogTheme
+                );
+                View bottomSheetView = LayoutInflater.from(view.getContext())
+                        .inflate(R.layout.bs_edit_profile, (LinearLayout)view.findViewById(R.id.bs_edit_profile_container));
+                bottomSheetView.findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
+
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                        view.getContext(), R.style.BottomSheetDialogTheme
+                );
+                View bottomSheetView = LayoutInflater.from(view.getContext())
+                        .inflate(R.layout.bs_change_password, (LinearLayout)view.findViewById(R.id.bs_change_password));
+                bottomSheetView.findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
+
+        favoritedJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(view.getContext(), favoritedJobs.class);
+                startActivity(intent);
+            }
+        });
+
+//        posts.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(ProfileUser.this, post.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(), "Logout nè >_<", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
