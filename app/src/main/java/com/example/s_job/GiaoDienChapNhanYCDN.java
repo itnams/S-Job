dcd0aac59ea3cfa99bc8115ab58537b1661609e9
VoDinhLong <vodinhlongit@gmail.com>
2020-11-21 09:21:21 +0700

    Chấp nhận yêu cầu doanh nghiệp

--- app/src/main/java/com/example/s_job/GiaoDienChapNhanYCDN.java
+++ app/src/main/java/com/example/s_job/GiaoDienChapNhanYCDN.java
@@ -1,12 +1,17 @@
 package com.example.s_job;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.s_job.Datacode.Account;
@@ -37,8 +42,8 @@ public class GiaoDienChapNhanYCDN extends AppCompatActivity {
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 Account account = dataSnapshot.getValue(Account.class);
                 String name = account.nameUser;
-                String sdt = account.passWord;
-                dstaikhoanyc.add(name + sdt);
+                String sdt = account.phone;
+                dstaikhoanyc.add(name + " " + sdt);
                 arrayAdapter.notifyDataSetChanged();
             }
 
@@ -64,5 +69,61 @@ public class GiaoDienChapNhanYCDN extends AppCompatActivity {
         });
          arrayAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item,dstaikhoanyc);
         listviewtk.setAdapter(arrayAdapter);
+        listviewtk.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(GiaoDienChapNhanYCDN.this);
+                builder1.setTitle("Vui lòng lựa chọn !");
+                builder1.setMessage("Click Accept để chấp nhận yêu cầu, Click Refuse để từ chối yêu cầu");
+                builder1.setCancelable(true);
+                builder1.setPositiveButton("Refuse",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                builder1.setNegativeButton("Accept",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                mData.child("Pending").addChildEventListener(new ChildEventListener() {
+                                    @Override
+                                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                                        Account account = dataSnapshot.getValue(Account.class);
+                                        String address = account.address;
+                                        String email = account.email;
+                                        String nameUser = account.nameUser;
+                                        String passWord = account.passWord;
+                                        String phone = account.phone;
+                                        String position = account.position;
+                                        Account account1 = new Account(address,email,nameUser,passWord,phone,position);
+                                        mData.child("User").child(email.replace("@gmail.com","")).setValue(account1);
+                                    }
+
+                                    @Override
+                                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                                    }
+
+                                    @Override
+                                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                                    }
+
+                                    @Override
+                                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                        });
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+            }
+        });
     }
 }
