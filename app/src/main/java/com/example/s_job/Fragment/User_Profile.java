c3da3f5b13595f914a9addb83ccd3fb1dc86f1d1
VoDinhLong <vodinhlongit@gmail.com>
2020-12-11 09:34:44 +0700

    fix

--- app/src/main/java/com/example/s_job/Fragment/User_Profile.java
+++ app/src/main/java/com/example/s_job/Fragment/User_Profile.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
