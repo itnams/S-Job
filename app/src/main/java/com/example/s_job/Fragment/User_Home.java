aacf3472ec1b6e296e4c9f420a3a72e1dcd967ae
Nam Nguyen <itnams12@gmail.com>
2020-12-17 08:22:44 +0700

    Update all post User

--- app/src/main/java/com/example/s_job/Fragment/User_Home.java
+++ app/src/main/java/com/example/s_job/Fragment/User_Home.java
@@ -10,15 +10,20 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.s_job.Activity_For_n.Create_Post_Company;
+import com.example.s_job.AllPost;
 import com.example.s_job.PostCompany;
 import com.example.s_job.PostCompanyAdapter;
 import com.example.s_job.R;
 import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -30,6 +35,7 @@ public class User_Home extends Fragment {
     ListView listViewPostCompany;
     ArrayList<PostCompany> arrayListPost;
     DatabaseReference mData;
+    Create_Post_Company create_post_company;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,20 +58,41 @@ public class User_Home extends Fragment {
         listViewPostCompany = view.findViewById(R.id.listPostCompany);
         arrayListPost = new ArrayList<PostCompany>();
         mData = FirebaseDatabase.getInstance().getReference();
-        arrayListPost.add(new PostCompany("a","b","c","d"));
-        arrayListPost.add(new PostCompany("s","s","s","s"));
-        arrayListPost.add(new PostCompany("a","b","c","d"));
+        mData.child("All-Post").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                AllPost allPost = snapshot.getValue(AllPost.class);
+                arrayListPost.add(new PostCompany(allPost.emai,allPost.key,allPost.ngayDang,allPost.tieuDe,allPost.tinhThanh));
+                PostCompanyAdapter adapter = new PostCompanyAdapter(
+                        getContext().getApplicationContext(),R.layout.dong_post_company,arrayListPost);
+                listViewPostCompany.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         ArrayList<String> arrayList = new ArrayList<String>();
-        arrayList.add("All");
-        arrayList.add("TP.HCM");
-        arrayList.add("Hà Nội");
-        arrayList.add("Đà Nẵng");
-        ArrayAdapter arrayAdapter = new ArrayAdapter(view.getContext(), R.layout.support_simple_spinner_dropdown_item, arrayList);
-        sprDiaDiem.setAdapter(arrayAdapter);
-        PostCompanyAdapter adapter = new PostCompanyAdapter(
-                getContext().getApplicationContext(),R.layout.dong_post_company,arrayListPost);
-        listViewPostCompany.setAdapter(adapter);
+        sprDiaDiem.setAdapter(new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_spinner_item, create_post_company.TinhThanhs));
+        sprDiaDiem.setSelection(0);
 
     }
 
