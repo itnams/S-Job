2ba12cb38b11cc48f085eed450e73e28d0681e14
nhok210724 <nhok210724@gmail.com>
2021-01-09 09:07:13 +0700

    Upload Imge and Load img online

--- app/src/main/java/com/example/s_job/db_firebase/dbFireBase.java
+++ app/src/main/java/com/example/s_job/db_firebase/dbFireBase.java
@@ -1,26 +1,39 @@
 package com.example.s_job.db_firebase;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.net.Uri;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.example.s_job.Model.Company;
 import com.example.s_job.Model.PostForCompany;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class dbFireBase {
     public FirebaseDatabase database;
     public DatabaseReference myRef;
-
+    public FirebaseStorage storage;
+    public StorageReference mStorageRef;
 
     public dbFireBase() {
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("Company");
+        //fire store
+
+        storage = FirebaseStorage.getInstance();
+        mStorageRef = storage.getReference();
 
     }
 
@@ -67,11 +80,11 @@ public class dbFireBase {
 
                     }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
 
-            }
-        });
+                    }
+                });
 
 
     }
@@ -92,4 +105,32 @@ public class dbFireBase {
         });
     }
 
+    public void UploadImgToFireBase(Uri img, String nameImg, Activity activity) {
+        ProgressDialog dialog = new ProgressDialog(activity);
+        dialog.setTitle("Upload...");
+        dialog.show();
+        mStorageRef.child(nameImg + ".jpg").putFile(img).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                dialog.dismiss();
+                Toast.makeText(activity, "Uploaded", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                dialog.dismiss();
+                Toast.makeText(activity, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+
+                double progress = (100.0 * snapshot.getBytesTransferred() / snapshot
+                        .getTotalByteCount());
+                dialog.setMessage("Uploaded " + (int) progress + "%");
+            }
+        });
+
+    }
 }
